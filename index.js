8575f36103767e3524345d9532eb49ccb437ea3d
Yu Li <yli0607x@gmail.com>
2018-07-31 23:24:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your functions and code here
-var window.kittens = ['Milo', 'Otis', 'Garfield']
+var kittens = ['Milo', 'Otis', 'Garfield']
 
 function destructivelyAppendKitten(Ralph){
-  return window.kittens.push(Ralph)
+  return kittens.push(Ralph)
 }
